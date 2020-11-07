74db99f545193f28102bda5c7f6059f7d3a1b797
Yun Kui Pan <yunpan@Yuns-MacBook-Pro.local>
2020-11-07 11:10:56 -0500

    server: relatcated files into /server; some cleanup on query results

--- server/db.ts
+++ server/db.ts
@@ -3,39 +3,60 @@ import { Pool, Client, QueryResult, QueryResultRow } from 'pg';
 const secret = require("./secret/secret.json")
 const pool = new Pool(secret)
 
-export async function listTables(schema='public') {
-  const res: QueryResult<QueryResultRow> = await pool.query(`select * from information_schema.tables where table_schema = '${schema}' and table_type = 'BASE TABLE'`)
-  // console.log("### tables: ", res.rows);
-  // console.log("### tables count: ", res.rowCount);
-  return res;
+export async function fetchTables(schema='public') {
+  const {rowCount, rows}: QueryResult<QueryResultRow> = await pool.query(`
+  select table_name 
+  from information_schema.tables 
+  where table_schema = '${schema}' 
+  and table_type = 'BASE TABLE'
+  `);
+  return {
+    count: rowCount, 
+    items: rows.map(row => row['table_name'])
+  }
 }
 
-export async function listColumns(schema='public', table) {
-  const res: QueryResult<QueryResultRow> = await pool.query(`select * from information_schema.columns where table_schema = '${schema}' and table_name = '${table}'`)
-  // console.log("### tables: ", res.rows);
-  // console.log("### tables count: ", res.rowCount);
-  return res;
+export async function fetchColumns(schema='public', table) {
+  const {rowCount, rows}: QueryResult<QueryResultRow> = await pool.query(`
+  select column_name, ordinal_position
+  from information_schema.columns 
+  where table_schema = '${schema}' 
+  and table_name = '${table}'
+  `);
+  return {
+    count: rowCount, 
+    items: rows.sort(colDef => colDef.ordinal_position).map(colDef => colDef['column_name'])
+  };
 }
 
-export async function listContraints(schema='public') {
-  const res: QueryResult<QueryResultRow> = await pool.query(`
-  SELECT rel.relname as table_name, rel.oid as table_id, 
-    con.conname as constraint, con.conname as column, 
-    con.confrelid as ref_Table_id, 
-    (select rel2.relname  from pg_catalog.pg_class rel2 where rel2.oid = con.confrelid) as ref_table_name,
-    con.contype as contraint_type,
-    con.conkey as self_columns_list, 
-    con.confkey as ref_columns_list
-       FROM pg_catalog.pg_constraint con
-            INNER JOIN pg_catalog.pg_class rel
-                       ON rel.oid = con.conrelid
-            INNER JOIN pg_catalog.pg_namespace nsp
-                       ON nsp.oid = connamespace
-       WHERE nsp.nspname = 'public'
-	   			and con.contype in ('f', 'p')
-		order by rel.relname asc, con.contype desc
-  `)
-  // console.log("### tables: ", res.rows);
-  // console.log("### tables count: ", res.rowCount);
-  return res;
+export async function fetchContraints(schema='public', table) {
+  const {rowCount, rows}: QueryResult<QueryResultRow> = await pool.query(`
+  SELECT
+    rel.relname AS table_name, 
+    -- rel.oid AS table_id, 
+    con.conname AS constraint, 
+    -- con.confrelid AS ref_Table_id, 
+    (SELECT rel2.relname 
+      FROM pg_catalog.pg_class rel2 
+      WHERE rel2.oid = con.confrelid) AS ref_table_name,
+    con.contype AS contraint_type,
+    con.conkey AS columns_index, 
+    con.confkey AS ref_columns_index
+  FROM 
+    pg_catalog.pg_constraint con
+    INNER JOIN pg_catalog.pg_class rel
+                ON rel.oid = con.conrelid
+    INNER JOIN pg_catalog.pg_namespace nsp
+                  ON nsp.oid = connamespace
+  WHERE 
+    nsp.nspname = '${schema}'
+    ${table ? `AND rel.relname = '${table}'` : ''}
+    AND con.contype IN ('f', 'p', 'u')
+  ORDER BY 
+    rel.relname asc, con.contype desc
+  `);
+  return {
+    count: rowCount,
+    items: rows
+  };
 }
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import express = require('express');
 import bodyParser from "body-parser";
-import {listTables, listColumns, listContraints} from "./db";
+import {fetchTables, fetchColumns, fetchContraints} from "./db";
 
 // Create a new express app instance
 const app: express.Application = express();
@@ -11,26 +11,27 @@ app.use(bodyParser.json())
 
 
 app.get('/api/tables', async (req, res) =>  {
-    const tbs = await listTables()
+    const tbs = await fetchTables()
     res.json(tbs);
 });
 
-app.get('/api/tables/table', async (req, res) => {
-    console.log()
-    const cols = await listColumns('public', 'address')
+app.get('/api/tables/:table', async (req, res) => {
+    const table = req.params['table']
+    const cols = await fetchColumns('public', table)
     res.json(cols);
 });
 
-app.get('/api/constraints', async (req, res) => {
-    const results = await listContraints('public')
+app.get('/api/constraints/:table', async (req, res) => {
+    const table = req.params['table'] || ''
+    const results = await fetchContraints('public', table)
     res.json(results);
 });
 
+app.get('/api/constraints', async (req, res) => {
+    const results = await fetchContraints('public', undefined)
+    res.json(results);
+});
 
 app.listen(3000, async function () {
-
-    
-    const constraints = await listContraints('public')
-    // console.log("server get tbs: ", tbs)
     console.log('App is listening on port 3000!');
 });

ad6ba3388020079edd7a6e76ce20b08cb4977f0b
Yun Pan <panyunkui@gmail.com>
2021-09-10 20:01:53 -0400

    updates run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,15 @@
-# kill port
-# kport 5000; kport 8001; kport 3001;
+# occupy ports 5000; 8001
 
 cd ./server
 npm install
 npm run build
-# kill port
-# kport 8001
 nohup node server.js &
-
-# npm start
+# For dev do: npm start
 
 cd ../client
 npm install
 npm run build
-# kill port
 
-# kport 5001
+# npm i -g serve
 serve -s build &
-# npm start
+# For dev do: npm start

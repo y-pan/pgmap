833df55ed44d58883ba77428308105aba9f9c1ee
Yun Kui Pan <panyunkui@gmail.com>
2021-10-16 22:24:11 -0400

    refactor: 1 - handle error

--- server/server.js
+++ server/server.js
@@ -20,47 +20,72 @@ app.options("*", cors({
 }));
 // routes
 // list databases
-app.get('/api/databases', async (req, res) => {
-    const databases = await (db_1.fetchDatabases());
-    res.json(databases);
+app.get('/api/databases', async (req, res, next) => {
+    try {
+        const databases = await (db_1.fetchDatabases());
+        res.json(databases);
+    }
+    catch (e) {
+        next(e);
+    }
 });
 // list schemas
-app.get("/api/schemas", async (req, res) => {
-    const database = req.query.database;
-    const schemas = await db_1.fetchSchemas(database); // schema names only
-    res.json(schemas);
+app.get("/api/schemas", async (req, res, next) => {
+    try {
+        const database = req.query.database;
+        const schemas = await db_1.fetchSchemas(database); // schema names only
+        res.json(schemas);
+    }
+    catch (e) {
+        next(e);
+    }
 });
 // list tables, optionally by schema
 app.get("/api/tables", async (req, res, next) => {
-    const schema = req.query.schema;
-    if (!schema) {
-        return res.status(400).send({ error: "Schema is required!" });
+    try {
+        const schema = req.query.schema;
+        if (!schema) {
+            throw new Error("Schema is required!");
+        }
+        const tables = await db_1.fetchTables(schema); // table names only
+        res.json(tables);
     }
-    const tables = await db_1.fetchTables(schema); // table names only
-    res.json(tables);
+    catch (e) {
+        next(e);
+    }
 });
 // list columns of a table
 app.get("/api/columns", async (req, res, next) => {
-    const schema = req.query.schema;
-    const table = req.query.table;
-    if (!schema) {
-        return res.status(400).send({ error: "Schema is required!" });
+    try {
+        const schema = req.query.schema;
+        const table = req.query.table;
+        if (!schema) {
+            throw new Error("Schema is required!");
+        }
+        const columns = await db_1.fetchColumnsByTable(schema, table);
+        res.json(columns);
     }
-    const columns = await db_1.fetchColumnsByTable(schema, table);
-    res.json(columns);
+    catch (e) {
+        next(e);
+    }
 });
 // list constraints, optionally by schema/table
-app.get("/api/constraints", async (req, res) => {
-    const schema = req.query.schema;
-    const table = req.query.table;
-    if (!schema) {
-        return res.status(400).send({ error: "Schema is required!" });
+app.get("/api/constraints", async (req, res, next) => {
+    try {
+        const schema = req.query.schema;
+        const table = req.query.table;
+        if (!schema) {
+            throw new Error("Schema is required!");
+        }
+        const results = await db_1.fetchContraints(schema, table);
+        res.json(results);
     }
-    const results = await db_1.fetchContraints(schema, table);
-    res.json(results);
+    catch (e) {
+        next(e);
+    }
 });
-app.get("*", async (req, res) => {
-    res.json({ message: "Bad request!" }).status(404);
+app.get("*", async (req, res, next) => {
+    next(new Error("Bad request!"));
 });
 // app run
 app.listen(serverPort, async function () {
--- server/server.ts
+++ server/server.ts
@@ -28,54 +28,73 @@ app.options(
 
 // routes
 // list databases
-app.get('/api/databases', async (req, res) => {
-  const databases = await(fetchDatabases())
-  res.json(databases);
+app.get('/api/databases', async (req, res, next) => {
+  try {
+    const databases = await(fetchDatabases())
+    res.json(databases);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // list schemas
-app.get("/api/schemas", async (req, res) => {
-  const database = req.query.database as string;
-  const schemas = await fetchSchemas(database); // schema names only
-  res.json(schemas);
+app.get("/api/schemas", async (req, res, next) => {
+  try {
+    const database = req.query.database as string;
+    const schemas = await fetchSchemas(database); // schema names only
+    res.json(schemas);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // list tables, optionally by schema
 app.get("/api/tables", async (req, res, next) => {
-  const schema = req.query.schema as string;
-  if (!schema) {
-    return res.status(400).send({ error: "Schema is required!" });
+  try {
+    const schema = req.query.schema as string;
+    if (!schema) {
+      throw new Error("Schema is required!");
+    }
+    const tables = await fetchTables(schema); // table names only
+    res.json(tables);
+  } catch (e) {
+    next(e);
   }
-  const tables = await fetchTables(schema); // table names only
-  res.json(tables);
 });
 
 // list columns of a table
 app.get("/api/columns", async (req, res, next) => {
-  const schema = req.query.schema as string;
-  const table = req.query.table as string;
-  if (!schema) {
-    return res.status(400).send({ error: "Schema is required!" });
+  try {
+    const schema = req.query.schema as string;
+    const table = req.query.table as string;
+    if (!schema) {
+      throw new Error("Schema is required!");
+    }
+    const columns = await fetchColumnsByTable(schema, table);
+    res.json(columns);
+  } catch (e) {
+    next(e);
   }
-
-  const columns = await fetchColumnsByTable(schema, table);
-  res.json(columns);
 });
 
 // list constraints, optionally by schema/table
-app.get("/api/constraints", async (req, res) => {
-  const schema = req.query.schema as string;
-  const table = req.query.table as string;
-  if (!schema) {
-    return res.status(400).send({ error: "Schema is required!" });
-  }
+app.get("/api/constraints", async (req, res, next) => {
+  try {
+    const schema = req.query.schema as string;
+    const table = req.query.table as string;
+    if (!schema) {
+      throw new Error( "Schema is required!");
+    }
 
-  const results = await fetchContraints(schema, table);
-  res.json(results);
+    const results = await fetchContraints(schema, table);
+    res.json(results);
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.get("*", async (req, res) => {
-  res.json({ message: "Bad request!" }).status(404);
+app.get("*", async (req, res, next) => {
+  next(new Error( "Bad request!"))
 });
 
 // app run
